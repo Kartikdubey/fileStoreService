4ac5ebf1d31f425b7059f6daff9fa836a4565633
Kartik Dubey <karthik.dubey111@gmail.com>
2021-10-24 11:07:55 +0530

    checked add functionality

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
-
-	"github.com/urfave/cli"
+	"net/http"
 )
 
 func main() {
-	err := cli.NewApp().Run(os.Args)
+	/*	err := cli.NewApp().Run(os.Args)
+		if err != nil {
+			log.Fatal(err)
+		}*/
+	fmt.Println("Client Started")
+	//appPort := "8000"
+	content, err := ioutil.ReadFile("abc.txt")
+	//var person Person
+	//err = json.Unmarshal(content, &person)
+	// json.Unmarshal Error
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
+	fmt.Println("Successfully Opened file passing it to server", content)
+
+	req, err := http.NewRequest("POST", "http://localhost:9000/add", bytes.NewBuffer(content))
+
+	if err != nil {
+		log.Panic(err)
+	}
+	req.Header.Set("Content-Type", "text/plain")
+	//req.Header.Set("fileType", "XML")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
 }
